8454c5d1cc8db65765e057bfb6cfc20896ae4183
Aleksandr Bobõrev <aleksandr.bobyrev@tthk.ee>
2018-09-12 18:27:15 +0300

    Add 2 new functions

--- Looping/main.cpp
+++ Looping/main.cpp
@@ -2,11 +2,24 @@
 #include <QDebug>
 #include <QString>
 
+void printLStars(int L);
+
+void print9Hello()
+{
+    qDebug() << "Hello from function print9Hello";
+
+    for (int i=0; i<9; i++){
+        printLStars(i);
+    }
+    qDebug() << "Exit from function print9Hello";
+}
+
+
+
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
-
-    int i=0, k=0;
+    print9Hello();
     /*
     for (i=0, k = 20; i<23; i++, k--){
         for (k=10; k>2; k--) {
@@ -14,7 +27,7 @@ int main(int argc, char *argv[])
          }
        qDebug() << "out print I is: " << i << " K is:" << k;
     }
-*/
+
 
     int s = 0;
     for (i=0; i<23; i++){
@@ -24,7 +37,15 @@ int main(int argc, char *argv[])
             qDebug() << message.arg(i).arg(k).arg(s);
             if (s>15) break;
         }
-    }
+    }*/
     qDebug() << "End";
     return a.exec();
+}
+
+void printLStars(int L){
+    QString qsOneRow;
+    for (int i=0; i<L; i++){
+        qsOneRow+='*';
+    }
+    qDebug() << qsOneRow;
 }
