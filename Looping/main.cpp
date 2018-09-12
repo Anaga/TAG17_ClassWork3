96306f7823bfd81c5bb090ef3cb689e695b0e3e3
Aleksandr Bobyrev <bobyrev.bob@gmail.com>
2018-09-12 23:10:57 +0300

    rename functions, add some comments

--- Looping/main.cpp
+++ Looping/main.cpp
@@ -1,41 +1,40 @@
 #include <QCoreApplication>
 #include <QDebug>
 #include <QString>
-void printLStars(int L);
-void print9Hello(){
-    qDebug() << "Hello from function print9Hello";
-    for (int i=0; i<9; i++){
-        printLStars(i);    }
-    qDebug() << "Exit from function print9Hello";
-}
 
-int main(int argc, char *argv[]){
+//functions headers delcaration, boath function return nothing - void
+void printMSpaseAndLStars(int M,int L); //Function header to print one row with M spases and L stars
+
+void printTriangle(int N); //Function header to print triangle and call function printMSpaseAndLStars N times
+
+// main program, function headers shall be declare before main
+int main(int argc, char *argv[])
+{
     QCoreApplication a(argc, argv);
-    print9Hello();
-    /*
-    for (i=0, k = 20; i<23; i++, k--){
-        for (k=10; k>2; k--) {
-            qDebug() << "inner printI is: " << i << " K is:" << k;
-         }
-       qDebug() << "out print I is: " << i << " K is:" << k;
-    }
 
+    int height = 5;
+    qDebug() << "Start, shall print triangle with"<< height << "rows";
+    printTriangle(height); // call function printTriangle with arg height
 
-    int s = 0;
-    for (i=0; i<23; i++){
-        for (k=0; k<10; k++) {
-            s= i+k;
-            QString message = "Summ of %1 + %2 = %3";
-            qDebug() << message.arg(i).arg(k).arg(s);
-            if (s>15) break;
-        }
-    }*/
     qDebug() << "End";
     return a.exec();
 }
-void printLStars(int L){
+
+void printMSpaseAndLStars(int M,int L) //Function body, M spases and L stars counts
+{
     QString qsOneRow;
-    for (int i=0; i<L; i++){
-        qsOneRow+='*';    }
-    qDebug() << qsOneRow;
+    for (int i=0; i<M; i++) qsOneRow+=' '; // += this is adding one char to end of string
+    for (int i=0; i<L; i++) qsOneRow+='*'; // += can be use and to concatinate 2 and more strings together
+
+    qDebug() << qsOneRow;                        // Now  show string borders
+    //qDebug() << qsOneRow.toLocal8Bit().data(); //don't show string borders
+}
+
+void printTriangle(int N) //Function body
+{
+    qDebug() << "Hello from function printTriangle";
+    for (int i=0; i<N; i++)    {
+        printMSpaseAndLStars(5, i);
+    }
+    qDebug() << "Exit from function printTriangle";
 }
